bb43703b53b0f801f7d5c98c90e2ec3e30fdd778
amsrocha2020 <amsrocha@outlook.com>
2024-08-31 16:28:10 +0100

    Added example to mock-api with push response

--- src/tests/api/api-mock.spec.ts
+++ src/tests/api/api-mock.spec.ts
@@ -39,4 +39,22 @@ test.describe('Mock data using Playwright - Fulfill ', () => {
     // Assert that the Strawberry fruit is visible
     await expect(page.getByText('Banana')).toBeVisible();
   });
+
+  test('gets the json from api and adds a new fruit', async ({ page }) => {
+    // Get the response and add to it
+    await page.route('*/**/api/v1/fruits', async (route) => {
+      const response = await route.fetch();
+      const json = await response.json();
+      json.push({ name: 'Loquat', id: 100 });
+      // Fulfill using the original response, while patching the response body
+      // with the given JSON object.
+      await route.fulfill({ response, json });
+    });
+
+    // Go to the page
+    await page.goto('https://demo.playwright.dev/api-mocking');
+
+    // Assert that the new fruit is visible
+    await expect(page.getByText('Loquat', { exact: true })).toBeVisible();
+  });
 });
