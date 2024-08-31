80ef91fc41aeb4e159f6c4e04ba71895e91c0998
amsrocha2020 <amsrocha@outlook.com>
2024-08-31 18:05:28 +0100

    Added step for more organization into --ui

--- src/pages/checkboxList.page.ts
+++ src/pages/checkboxList.page.ts
@@ -1,35 +1,40 @@
-import { Locator, Page } from '@playwright/test'
+import { Locator, Page } from '@playwright/test';
+import { test } from '../fixtures';
 
 export class CheckboxPage {
-  readonly page: Page
-  readonly firstCheckbox: Locator
-  readonly secondCheckbox: Locator
+  readonly page: Page;
+  readonly firstCheckbox: Locator;
+  readonly secondCheckbox: Locator;
 
   constructor(page: Page) {
-    this.page = page
-    this.firstCheckbox = page.getByRole('checkbox').first()
-    this.secondCheckbox = page.getByRole('checkbox').nth(1)
+    this.page = page;
+    this.firstCheckbox = page.getByRole('checkbox').first();
+    this.secondCheckbox = page.getByRole('checkbox').nth(1);
   }
 
   // Method to check the first checkbox
   async checkFirstCheckbox() {
-    await this.firstCheckbox.check()
+    await test.step('Check first checkbox', async () => {
+      await this.firstCheckbox.check();
+    });
   }
 
   // Method to uncheck the second checkbox
   async uncheckSecondCheckbox() {
-    if (await this.secondCheckbox.isChecked()) {
-      await this.secondCheckbox.uncheck()
-    }
+    await test.step('Uncheck second checkbox', async () => {
+      if (await this.secondCheckbox.isChecked()) {
+        await this.secondCheckbox.uncheck();
+      }
+    });
   }
 
   // Method to verify if the first checkbox is checked
   async isFirstCheckboxChecked(): Promise<boolean> {
-    return await this.firstCheckbox.isChecked()
+    return await this.firstCheckbox.isChecked();
   }
 
   // Method to verify if the second checkbox is unchecked
   async isSecondCheckboxUnchecked(): Promise<boolean> {
-    return !(await this.secondCheckbox.isChecked())
+    return !(await this.secondCheckbox.isChecked());
   }
 }
